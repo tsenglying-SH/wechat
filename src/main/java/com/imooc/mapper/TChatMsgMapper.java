a4f0942d3b8ab0523cf2ee0877014054bd043925
unknown <you@example.com>
2020-07-23 18:45:24 +0800

    generator

--- src/main/java/com/imooc/mapper/TChatMsgMapper.java
+++
@@ -1,6 +0,0 @@
-package com.imooc.mapper;
-
-import com.jiuting.utils.MyMapper;
-
-public interface TChatMsgMapper extends MyMapper<TChatMsg> {
-}
--- src/main/java/com/imooc/mapper/TMyFriendMapper.java
+++
@@ -1,6 +0,0 @@
-package com.imooc.mapper;
-
-import com.jiuting.utils.MyMapper;
-
-public interface TMyFriendMapper extends MyMapper<TMyFriend> {
-}
